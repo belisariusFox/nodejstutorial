c0c8a4d8a954bdfc42a480bc28788e40f83e0aed
Luca Volpe <vluca.pro@gmail.com>
2023-09-22 17:40:16 +0000

    build

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,11 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', test : 'Luca', number: 117, link:'/test' });
+  res.render('index', { title: 'Express', test : 'Luca', number: 117, link:'/users', array:[
+    'elemento 1',
+    'elemento 2',
+    'elemento 3',
+    'elemento 4',
+    'elemento 5',
+    'elemento 6',
+  ] });
 });
 
 router.get('/test', function(req, res, next) {
   res.render('test');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title:'Login'});
+});
+
+router.get('/login-studente', function(req, res, next) {
+  res.render('studente', {title:'Login studente'});
 });
 
 
