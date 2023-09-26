9e3aa211196d4011ebbff49051f343831384313b
Luca Volpe <vluca.pro@gmail.com>
2023-09-26 14:40:08 +0000

    new

--- server/routes/session.js
+++ server/routes/session.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const multipart = require("connect-multiparty");
+const multipartMiddleware= multipart({uploadDir:'./uploads'});
+
 
 router.post('/', function (req, res, next) {
     res.render('account', { title: 'Account', nickname: 'Luca', date: new Date });
+});
+
+router.get('/upload', function (req, res, next) {
+    res.render('upload', { title: 'Upload test' });
 });
 
 
